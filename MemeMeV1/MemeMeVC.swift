a25bdbc565f8c1bae1a4ad76ef6977b120f4e295
Pierre Younes <pyounes@gmail.com>
2021-01-17 19:05:43 +0200

    add imageview and toolbar item and connecting buttons

--- MemeMeV1/MemeMeVC.swift
+++ MemeMeV1/MemeMeVC.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class MemeMeVC: UIViewController {
 
+    // MARK: - Outlets
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // MARK: - Image From Camera
+    @IBAction func pickImageFromCamera(_ sender: UIBarButtonItem) {
+    }
+    
+    // MARK: - Image From Gallery
+    @IBAction func pickImageFromGallery(_ sender: Any) {
+    }
+    
 }
 
